77d4d5cb631fe31ea1334f9b396765b6eb4603cf
prabinthapa10 <prabinmagar636@gmail.com>
2024-10-06 16:18:31 +0545

    making api call

--- recipeApp/src/components/Search.jsx
+++ recipeApp/src/components/Search.jsx
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
 
-export default function Search() {
-  const [query, setQuery] = useState("");
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "46bea26973144f3eb5c4ae166906e3ad"; //must define in envirmoment
 
+export default function Search() {
+  const [query, setQuery] = useState("pizza");
+
+  //   syntax of use effect hook
+  useEffect(() => {
+    // async to make await untile the data is fetch
+    async function fetchFood() {
+      const response = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await response.json();
+      console.log(data.results);
+    }
+    fetchFood();
+  }, []);
   return (
     <div>
       <input
